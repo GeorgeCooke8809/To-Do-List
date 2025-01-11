7c50d08142ef8e277efa3cebbd83dcb806799db0
George Cooke <georgecooke8809@gmail.com>
2025-01-11 21:17:18 +0000

    Added recurring items

--- To Do List/To Do List/To_Do_List.py
+++ To Do List/To Do List/To_Do_List.py
@@ -1,5 +1,7 @@
 from pickle import NONE
 import sqlite3
+import datetime
+import calendar
 
 data = sqlite3.connect("ToDo.db")
 cursor = data.cursor()
@@ -58,16 +60,71 @@ def add_new():
         else:
             print("ERROR: Invalid Input")
             priority = ""
+    
+    print()
+
+    print("Recurring Type:")
+    print("1 - None")
+    print("2 - Weekly")
+    print("3 - Monthly")
+
+    print()
+
+    recurring_type = input()
+
+    if recurring_type == "2":
+        print()
+        print("Set Weekly Recurring:")
+        print("1 - Monday")
+        print("2 - Turesday")
+        print("3 - Wednesday")
+        print("4 - Thursday")
+        print("5 - Friday")
+        print("6 - Saturday")
+        print("7 - Sunday")
+
+        recurring = input()
+
+        if recurring == "1":
+            recurring_on = "Monday"
+        elif recurring == "2":
+            recurring_on = "Tuesday"
+        elif recurring == "3":
+            recurring_on = "Wednesday"
+        elif recurring == "4":
+            recurring_on = "Thursday"
+        elif recurring == "5":
+            recurring_on = "Friday"
+        elif recurring == "6":
+            recurring_on = "Saturday"
+        elif recurring == "7":
+            recurring_on = "Sunday"
+
+        recurring = "w"
+
+    elif recurring_type == "3":
+        print()
+        print("Select Day of Month:")
+        recurring_on = int(input())
+        recurring = "m"
+
+    else:
+        recurring = "n"
+
+    if recurring != "n":
+        today = datetime.date.today()
+        yesterday = today - datetime.timedelta(days = 1)
 
     cursor.execute("SELECT COUNT(*) FROM ToDo")
     total_items = int(cursor.fetchone()[0])
     ID = total_items + 1
 
-    entry = (ID, name, description, status, category, priority, "", "", "")
+    entry = (ID, name, description, category, status, priority, recurring, recurring_on, yesterday)
 
     cursor.execute("INSERT INTO ToDo VALUES (?,?,?,?,?,?,?,?,?)", entry)
     data.commit()
 
+    print()
 
     get_items()
 
@@ -79,21 +136,97 @@ def get_items():
 
     for i in range(0,total_items):
         row = i+1
-        cursor.execute("SELECT Title FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
-        name = cursor.fetchone()
+        cursor.execute("SELECT Recurring FROM ToDo WHERE ID =:c", {"c": row})
+        recurring = cursor.fetchone()
+        if recurring != None:
+            recurring = recurring[0]
+        if recurring == "n":
+            cursor.execute("SELECT Title FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
+            name = cursor.fetchone()
+            if name != None:
+                name = name[0]
+            cursor.execute("SELECT Priority FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
+            priority = cursor.fetchone()
+            if priority != None:
+                priority = priority[0]
+            cursor.execute("SELECT Status FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
+            status = cursor.fetchone()
+            if status != None:
+                status = status[0]
+       
+        elif recurring == "w":
+            today = datetime.date.today()
+            day_of_week = calendar.day_name[today.weekday()]
+            
+            cursor.execute("SELECT RecurringOn FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
+            recurring_on = cursor.fetchone()
+            if recurring_on != None:
+                recurring_on = recurring_on[0]
+
+            if day_of_week == recurring_on:
+                cursor.execute("SELECT LastComplete FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
+                last_complete = cursor.fetchone()
+                if last_complete != None:
+                    last_complete = last_complete[0]
+
+                last_complete = datetime.datetime.strptime(last_complete, '%Y-%m-%d').date()
+                if last_complete != today:
+                    cursor.execute("SELECT Title FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
+                    name = cursor.fetchone()
+                    if name != None:
+                        name = name[0]
+                    cursor.execute("SELECT Priority FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
+                    priority = cursor.fetchone()
+                    if priority != None:
+                        priority = priority[0]
+                    cursor.execute("SELECT Status FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
+                    status = cursor.fetchone()
+                    if status != None:
+                        status = status[0]
+                else:
+                    name = None
+            else:
+                name = None
+        elif recurring == "m":
+            today = datetime.date.today()
+            day_of_month = str(today.day)
+            
+            cursor.execute("SELECT RecurringOn FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
+            recurring_on = cursor.fetchone()
+            if recurring_on != None:
+                recurring_on = recurring_on[0]
+
+            if recurring_on == day_of_month:
+                cursor.execute("SELECT LastComplete FROM ToDo WHERE ID =:c AND STATUS != 'Complete'", {"c": row})
+                last_complete = cursor.fetchone()
+                if last_complete != None:
+                    last_complete = last_complete[0]
+                last_complete = datetime.datetime.strptime(last_complete, '%Y-%m-%d').date()
+
+
+                if last_complete != today:
+                    cursor.execute("SELECT Title FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
+                    name = cursor.fetchone()
+                    if name != None:
+                        name = name[0]
+                    cursor.execute("SELECT Priority FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
+                    priority = cursor.fetchone()
+                    if priority != None:
+                        priority = priority[0]
+                    cursor.execute("SELECT Status FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
+                    status = cursor.fetchone()
+                    if status != None:
+                        status = status[0]
+                else:
+                    name = None
+            else:
+                name = None
+        else:
+            name = None
+
         if name != None:
-            name = name[0]
-        cursor.execute("SELECT Priority FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
-        priority = cursor.fetchone()
-        if priority != None:
-            priority = priority[0]
-        cursor.execute("SELECT Category FROM ToDo WHERE ID =:c AND Status != 'Complete'", {"c": row})
-        category = cursor.fetchone()
-        if category != None:
-            category = category[0]
-        
-        if name != None:
-            print(row, "-", name, "-", category, "- Priority:", priority)
+            print(row, "-", name, "-", status, "- Priority:", priority)
+
 
     print("000 - Options")
 
@@ -179,8 +312,16 @@ def get_items():
             print()
             get_items()
         elif option_in == "1":
-            cursor.execute("UPDATE ToDo SET Status = 'Complete' WHERE ID =:c", {"c": item_editing})
-            data.commit()
+            if recurring == "n":                
+                cursor.execute("UPDATE ToDo SET Status = 'Complete' WHERE ID =:c", {"c": item_editing})
+                data.commit()
+            else:
+                cursor.execute("UPDATE ToDo SET Status = 'Not Started' WHERE ID =:c", {"c": item_editing})
+                data.commit()
+
+                cursor.execute("UPDATE ToDo Set LastComplete =:d WHERE ID =:c", {"d": today, "c": item_editing})
+                data.commit()
+
             print()
             get_items()
         elif option_in == "2":
@@ -213,8 +354,16 @@ def get_items():
                 print()
                 get_items()
             elif status_to == 4:
-                cursor.execute("UPDATE ToDo SET Status = 'Complete' WHERE ID =:c", {"c": item_editing})
-                data.commit()
+                if recurring == "n":                
+                    cursor.execute("UPDATE ToDo SET Status = 'Complete' WHERE ID =:c", {"c": item_editing})
+                    data.commit()
+                else:
+                    cursor.execute("UPDATE ToDo SET Status = 'Not Started' WHERE ID =:c", {"c": item_editing})
+                    data.commit()
+
+                    cursor.execute("UPDATE ToDo Set LastComplete =:d WHERE ID =:c", {"d": today, "c": item_editing})
+                    data.commit()
+
                 print()
                 get_items()
             elif status_to == 0:
